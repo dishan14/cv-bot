0f4ca267c1441a8aa8952dde31f63f9929bcce11
dishan14 <dishanshukla1410@gmail.com>
2025-12-30 21:38:27 +0000

    retrieval changes

--- rag-cv-bot/app.py
+++ rag-cv-bot/app.py
@@ -4,7 +4,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_groq import ChatGroq
-from langchain.chains import RetrievalQA
+from langchain_classic.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 
 # 1 Load docs
